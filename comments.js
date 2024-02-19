adca724906405a0ecf84e553a6b710ba37f1fe2d
ZayJackRadovan <77386908+ZayJackPrvy@users.noreply.github.com>
2024-02-19 08:19:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,6 @@
 // create web server
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/comments');
+
+router.get('/', commentController);
